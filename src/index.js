e9e3130e1d0a5a2b5cc4196d21fff812f9964e4f
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-04 20:53:49 +0530

    trial2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+<BrowserRouter>
+<App />
+</BrowserRouter>
+</Provider>
+, document.getElementById("root"));
